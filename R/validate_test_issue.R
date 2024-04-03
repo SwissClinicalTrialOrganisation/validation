ec05ab130c70b034eb014d58286ee9de2948d358
Alan Haynes <aghaynes@users.noreply.github.com>
2024-04-03 15:55:53 +0200

    add validate_test_issue

---
+++ R/validate_test_issue.R
@@ -0,0 +1,40 @@
+#' validate the inputs for a package validation report
+#' @param score the output from calculate_pkg_score for a single package
+#' @return list wit two elements: score_ok (logical) and message (character)
+#' @keywords internal
+validate_test_issue <- function(issue){
+  message <- NULL
+  if(is.na(issue$package)){
+    message <- c(message, "Please enter a package name")
+  }
+  if(is.na(issue$version)){
+    message <- c(message, "Please enter a package version")
+  }
+  if(is.na(issue$test_date)){
+    message <- c(message, "Please enter a test date")
+  }
+  if(is.na(issue$tests)){
+    message <- c(message, "Please enter a test description")
+  }
+  if(is.na(issue$test_result)){
+    message <- c(message, "Please enter a test results (PASS or FAIL)")
+  }
+  if(is.na(issue$test_evidence)){
+    message <- c(message, "Please enter evidence of test results")
+  }
+  if(is.na(issue$session_info)){
+    message <- c(message, "Please enter session info")
+  }
+  if(is.na(issue$session_info)){
+    message <- c(message, "Please provide a location where code is saved")
+  }
+
+  if(is.null(message)){
+    list(ok = TRUE,
+         message = "")
+
+  } else {
+    list(ok = FALSE,
+         message = paste(message, collapse = "\n\n"))
+  }
+}
