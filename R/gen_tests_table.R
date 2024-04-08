d844ea741a8dcfe033356a2dbba1cb8efae29545
Alan Haynes <aghaynes@users.noreply.github.com>
2024-04-08 15:06:35 +0200

    clear warnings from R CMD CHECK

--- R/gen_tests_table.R
+++ R/gen_tests_table.R
@@ -2,7 +2,7 @@
 #'
 #' This is intended for use by a github action to generate a table for the website
 #'
-#' @param pkgs NULL or a list of issues from \code{get_issues}
+#' @param tests NULL or a list of issues from \code{get_issues}
 #' @param ... options passed to \code{get_issues}
 #' @export
 #' @describeIn gen_pkg_table Generate a table of function tests
