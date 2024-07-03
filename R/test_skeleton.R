33feb172627896c0aa09d804c28d25322e76b009
Alan Haynes <aghaynes@users.noreply.github.com>
2024-07-03 16:19:10 +0200

    add testthat version info

--- R/test_skeleton.R
+++ R/test_skeleton.R
@@ -41,7 +41,10 @@ test_skeleton <- function(pkg, funs, dir = getwd()){
       write(c("# Write relevent tests for the function in here",
               "# Consider the type of function:",
               "#   - is it deterministic or statistic?",
-              "#   - is it worth checking for errors/warnings under particular conditions?"),
+              "#   - is it worth checking for errors/warnings under particular conditions?",
+              "local_edition(3)",
+              "",
+              ""),
             file = file,
             sep = "\n")
     } else {
