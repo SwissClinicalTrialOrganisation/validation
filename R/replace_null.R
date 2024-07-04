e1635572375fed32491481599fb0488fde66ae36
Alan Haynes <aghaynes@users.noreply.github.com>
2024-07-04 08:32:14 +0200

    internal - no rd

--- R/replace_null.R
+++ R/replace_null.R
@@ -2,11 +2,13 @@
 #' @noRd
 replace_null <- function(object, ...) UseMethod("replace_null", object)
 
+#' @noRd
 replace_null.default <- function(issue){
   lapply(issue, as.character) |>
     lapply(function(x) dplyr::if_else(!(is.null(x) | length(x) == 0), x, ""))
 }
 
+#' @noRd
 replace_null.list <- function(x){
   lapply(x, replace_null)
 }
